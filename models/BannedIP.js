d8e86baad689b0036ee9a5ff1bcf4d5406bf973e
medrean <medrean1m@gmail.com>
2026-09-21 17:22:50 +0300

    Delete models directory

--- models/BannedIP.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const bannedIPSchema = new mongoose.Schema({
-    ip: { type: String, required: true, unique: true },
-    reason: { type: String, default: 'حظر يدوي من اللوحة' },
-    createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('BannedIP', bannedIPSchema);
